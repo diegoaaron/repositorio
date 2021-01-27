dbbfad0a07d3e0b83e38c26eac7066f94b455c66
diego <diegoaarondv@gmail.com>
2021-01-26 19:22:08 -0500

    update vue.js 4

--- vue.js/apuntes/vueone/app.js
+++ vue.js/apuntes/vueone/app.js
@@ -1,12 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'https://www.google.com.pe',
             showBooks: true,
-            title: 'The final empirez',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false},
+                {title: 'the final empire', author: 'erick wolf', img: 'assets/3.jpg', isFav: true},
+            ]
         }
     },
     methods: {
@@ -16,18 +17,19 @@ const app = Vue.createApp({
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
         },
-        handleEvent(e, data) {
-            console.log(e, e.type);
-
-            if(data) {
-                console.log(data);
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
+        toggleFav(book) {
+            book.isFav = !book.isFav;
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
 
 app.mount('#app')
+
+// Challenge -- add to Favs
+//      attach a click event to each li tag (for each book)
+//      when a user clicks an li, toggle the 'isFav' property of that book
