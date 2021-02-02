f67cbeb6506fb4ef4c93737bbcfddbabe3aaca1f
diego <diegoaarondv@gmail.com>
2021-02-02 18:03:36 -0500

    update vue.js 9

--- vue.js/proyectos/reaction-timer/src/App.vue
+++ vue.js/proyectos/reaction-timer/src/App.vue
@@ -1,23 +1,35 @@
 <template>
 <h1>Ninja Reaction Timer</h1>
-<button @click="start">play</button>
+<button @click="start" :disabled="isPlaying">play</button>
+<Block v-if="isPlaying" :delay="delay" @end="endGame" />
+<p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
 
+import Block from './components/Block.vue';
+
 export default {
   name: 'App',
-  components: { },
+  components: { Block },
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
     }
   }
 }
