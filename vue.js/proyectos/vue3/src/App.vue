d91c69df81ab5ee17fd053fe83c996aaa53a44ff
diego <diegoaarondv@gmail.com>
2021-02-13 23:06:32 -0500

    update vue.js 18

---
+++ vue.js/proyectos/vue3/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Dc Heros</h1>
+  <ul>
+    <li v-for="num in 10" :key="num">{{ num }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dcHeros: ["Supergirl", "Flash", "Batman"],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
