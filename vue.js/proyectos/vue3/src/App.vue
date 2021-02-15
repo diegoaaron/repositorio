4cc72aa47cc44df81927ac90aa446f87df106fe9
diego <diegoaarondv@gmail.com>
2021-02-14 21:47:24 -0500

    update vue.js 20

--- vue.js/proyectos/vue3/src/App.vue
+++ vue.js/proyectos/vue3/src/App.vue
@@ -1,20 +1,37 @@
 <template>
-  <h1>Dc Heros</h1>
+  <h1>Dc Heros {{ herosCount }} </h1>
+
   <ul>
-    <li v-for="hero in dcHeros" :key="hero">{{ hero.name }}</li>
+    <li v-for="(hero, index) in dcHeros" :key="hero.name">
+      <div>{{ hero.name }} <button @click="remove(index)">x</button> </div>
+    </li>
   </ul>
 
-  <form @submit.prevent="dcHeros.push({ name: newHero });
-  newHero = '';">
+  <form @submit.prevent="addHero">
     <input v-model="newHero" placeholder="Type Hero Name here">
     <button type="submit">Add Hero</button>
   </form>
-  
 
 </template>
 
 <script>
 export default {
+  computed: {
+    herosCount() {
+      return this.dcHeros.length + " heros";
+    },
+  },
+  methods: {
+    addHero() {
+      if(this.newHero !== '') {
+        this.dcHeros.push({ name: this.newHero });
+        this.newHero = "";
+      }
+    },
+    remove(index) {
+      this.dcHeros = this.dcHeros.filter((hero, i) => i != index);
+    },
+  },
   data() {
     return {
       newHero: "",
