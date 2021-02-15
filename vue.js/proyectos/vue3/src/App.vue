10e9ef9207d3a67ec6bf000877d2365087d665ef
diego <diegoaarondv@gmail.com>
2021-02-14 20:24:17 -0500

    update vue.js 19

--- vue.js/proyectos/vue3/src/App.vue
+++ vue.js/proyectos/vue3/src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <h1>Dc Heros</h1>
   <ul>
-    <li v-for="num in 10" :key="num">{{ num }}</li>
+    <li v-for="hero in dcHeros" :key="hero">{{ hero.name }}</li>
   </ul>
+
+  <form @submit.prevent="dcHeros.push({ name: newHero });
+  newHero = '';">
+    <input v-model="newHero" placeholder="Type Hero Name here">
+    <button type="submit">Add Hero</button>
+  </form>
+  
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      dcHeros: ["Supergirl", "Flash", "Batman"],
+      newHero: "",
+      dcHeros: [{name: "Supergirl"}, {name: "Flash"}, {name: "Batman"}, {name: "Arrow"}, {name: "Superman"}],
     }
   }
 }
