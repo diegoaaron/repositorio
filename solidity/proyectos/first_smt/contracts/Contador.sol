be2900d09b211c274bb07e343abb85afedb01c58
diego <diegoaarondv@gmail.com>
2021-02-18 18:50:39 -0500

    update solidity 13

---
+++ solidity/proyectos/first_smt/contracts/Contador.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+
+pragma solidity 0.7.5;
+
+contract Contador {
+    uint256 count;
+
+    constructor (uint256 _count) {
+        count = _count;
+    }
+
+    
+}
