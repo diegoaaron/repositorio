00da37d3b082324d105a7f0c32e73e564ba9cfd5
diego <diegoaarondv@gmail.com>
2021-02-17 21:30:03 -0500

    update vue.js 24

--- vue.js/proyectos/vue3/src/pages/Markdown.vue
+++ vue.js/proyectos/vue3/src/pages/Markdown.vue
@@ -1,10 +1,42 @@
 <template>
-  hola
+  <div class="flex flex-wrap w-full">
+    <h1 class="w-full text-center text-3xl my-4">Markdown App</h1>
+    <section class="flex m-auto w-10/12 h-screen">
+
+      <article class="w-1/2 border">
+        <textarea class="w-full h-full" :value="text" @input="update"></textarea>
+      </article>
+
+      <article class="w-1/2 border bg-gray-100" v-html="markedText"></article>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
+import marked from "marked";
+import debounce from '../utilities/mixins/debounce';
 
+export default {
+  data() {
+    return {
+      text: "",
+      timeout: "",
+    };
+  },
+  computed: {
+    markedText() {
+      return marked(this.text);
+    },
+  },
+  methods: {
+    update(e){},
+    debounce(func, wait = 1000){
+      clearTimeout(this.timeout);
+        this.timeout = setTimeout(() => {
+          this.text = e.target.value;
+        }, 500);
+    },
+  },
 }
 </script>
 
