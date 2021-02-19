56a413f8cd28f53de7e88d68ad60e97ef6b1cb73
diego <diegoaarondv@gmail.com>
2021-02-19 18:06:56 -0500

    update solidity 14

--- solidity/proyectos/first_smt/contracts/Contador.sol
+++ solidity/proyectos/first_smt/contracts/Contador.sol
@@ -9,5 +9,20 @@ contract Contador {
         count = _count;
     }
 
+    function setCount(uint256 _count) public {
+        count = _count;
+    }
+
+    function incrementCount() public {
+        count += 1;
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
+    function getNumber() public pure returns(uint256) {
+        return 34;
+    }
     
 }
