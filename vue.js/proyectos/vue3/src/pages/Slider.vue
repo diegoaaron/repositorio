f2bb6449c322386fbbcfb4dffc888acaeb66aa6d
diego <diegoaarondv@gmail.com>
2021-02-18 20:44:46 -0500

    update vue.js 26

--- vue.js/proyectos/vue3/src/pages/Slider.vue
+++ vue.js/proyectos/vue3/src/pages/Slider.vue
@@ -1,8 +1,23 @@
 <template>
-    <div class="flex flex-wrap w-full">
-        <div v-if="currentSlide == 0" class="absolute w-full bg-blue-600" style="height: 350px"></div>
-        <div v-if="currentSlide == 1" class="absolute w-full bg-yellow-600" style="height: 350px"></div>
-        <div v-if="currentSlide == 2" class="absolute w-full bg-green-600" style="height: 350px"></div>
+    <div class="flex flex-wrap w-full relative">
+        <div class="absolute w-full" v-for="(color, index) in sliders" :key="color">
+            <transition name="fade">
+                <div v-if="currentSlide == index" :class="color" style="height: 350px"></div>
+            </transition>
+        </div>
+        <div class="w-full" style="height:340px">
+            <div class="absolute bottom-0 w-full flex justify-center">
+                <div v-for="(slider, index) in sliders" :key="slider" @click="makeActive(index)" :class="currentSlide == index ? 'bg-gray-700' : 'bg-gray-300'" class="w-4 h-4 mx-2 rounded-full cursor-pointer shadow-md"></div>
+            </div>
+        </div>
+        <!--<div class="my-10 flex w-full">
+            <div class="m-auto">
+                    <transition name="fade">
+                        <h1 v-if="isTitleShowing">Slider Carousel</h1>
+                    </transition>
+                <button @click="isTitleShowing = !isTitleShowing" class="px-2 rounded border">Toggle Text</button>
+            </div>
+        </div>-->
     </div>
 </template>
 
@@ -11,16 +26,19 @@ export default {
     data() {
         return {
             currentSlide: 0,
+            sliders: ["bg-blue-600", "bg-yellow-600", "bg-green-600"],
             interval: "",
+            isTitleShowing: true,
         };
+    },
+    methods: {
+        makeActive(index) {
+            this.currentSlide = index;
+        },
     },
     mounted() {
         this.interval =  setInterval(() => {
-            if(this.currentSlide === 2) {
-                this.currentSlide = 0;
-            } else {
-                this.currentSlide++;
-            }
+            this.currentSlide = this.currentSlide === 2 ? 0 : this.currentSlide + 1;
         }, 2000);
     },
     beforeUnmount() {
@@ -31,5 +49,16 @@ export default {
 </script>
 
 <style>
-
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s ease;
+}
+.fade-enter-from {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
 </style>
