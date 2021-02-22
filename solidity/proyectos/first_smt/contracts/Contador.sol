08a60da9c9e9a261a02d0719b518b35d84a63e73
diego <diegoaarondv@gmail.com>
2021-02-22 18:07:58 -0500

    update solidity 18

--- solidity/proyectos/first_smt/contracts/Contador.sol
+++ solidity/proyectos/first_smt/contracts/Contador.sol
@@ -1,27 +1,32 @@
 //SPDX-License-Identifier: Unlicense
-
 pragma solidity 0.7.5;
+
+import "hardhat/console.sol";
 
 contract Contador {
     uint256 count;
 
     constructor (uint256 _count) {
         count = _count;
+        console.log("El valor del contador al desplegar es: ", count);
     }
 
     function setCount(uint256 _count) public {
         count = _count;
+        console.log("El nuevo valor del contador es: ", count);
     }
 
     function incrementCount() public {
         count += 1;
+        console.log("El contador despues de incrementar es: ", count);
     }
 
     function getCount() public view returns (uint256) {
+        console.log("el valor actual del contador es: ", count);
         return count;
     }
 
-    function getNumber() public pure returns(uint256) {
+    function getNumber() public pure returns (uint256) {
         return 34;
     }
     
--- solidity/proyectos/first_smt/test/contador_test.js
+++ solidity/proyectos/first_smt/test/contador_test.js
@@ -1,8 +1,19 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function() {
+describe("Contador", function() {
     it("Definimos el valor inicial del contador", async function() {
-        const Contador = await ethers.
-    })
-})
+        const Contador = await ethers.getContractFactory("Contador"); 
+        const contador = await Contador.deploy(100);
+
+        await contador.deployed();
+
+
+        expect(await contador.getCount()).to.equal(100); //validando el constructor
+
+        expect(await contador.getNumber()).to.equal(34);
+        
+        
+        
+    });
+});
