126870a3b24e20aca3c9451bbf1f287fcd79b688
diego <diegoaarondv@gmail.com>
2021-02-24 22:14:47 -0500

    update vue.js 33

--- vue.js/proyectos/vue3/src/components/Modal.vue
+++ vue.js/proyectos/vue3/src/components/Modal.vue
@@ -1,9 +1,16 @@
 <template>
-    <section class="z-20 h-screen w-screen bg-gray-500 fixed top-0 left-0 opacity-50"></section>
+    <section @click="close" class="z-20 h-screen w-screen bg-gray-500 fixed top-0 left-0 opacity-50"></section>
     <div class="absolute inset-0">
         <div class="flex h-full">
             <div class="z-30 m-auto bg-white p-2 rounded shadow w-1/3">
-                <div class="p-2 border"></div>
+                <div class="p-2 border">
+                    <h1 class="text-2xl">
+                        <slot name="title"></slot>
+                    </h1>
+                    <main>
+                        <slot name="body"></slot>
+                    </main>
+                </div>
             </div>
         </div>
     </div>
@@ -12,9 +19,9 @@
 <script>
 
 export default {
-    setup() {
+    setup(_, { emit }) {
         function close() {
-            $emit("close-login");
+            emit("close");
         }
 
         return { close };
