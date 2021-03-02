d928aac71093da2b22c61cf666384417fc71114c
diego <diegoaarondv@gmail.com>
2021-03-02 11:23:35 -0500

    update python 1

---
+++ python/proyectos/repaso/README.md
@@ -0,0 +1,16 @@
+## Repaso
+
+**resumen**: scripts de repaso de código
+
+### Puntos revisados
+
+- variables
+- operadores
+- sentencias condicionales
+- 
+
+### Herramientas utilizadas
+
+- python 3.8
+- [guía utilizada](https://www.freecodecamp.org/news/the-ultimate-guide-to-python-from-beginner-to-intermediate-to-pro/)
+
---
+++ python/proyectos/repaso/basico_uno.py
@@ -0,0 +1,48 @@
+num = 5.0
+print(num)
+print(type(num))
+
+greet = "Hello user"
+print(greet)
+print(type(greet))
+
+is_available = True
+print(is_available)
+print(type(is_available))
+
+num = None
+print(num)
+print(type(num))
+
+a = 6
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+
+a = 7
+b = 3
+
+print(a % b)
+print(a // b)
+
+a = 5
+b = 2
+
+print(a>b)
+print(a<b)
+print(a==b)
+print(a>=5)
+print(b<=1)
+
+a = 10
+b = 2
+
+print(a == 2 and b == 10)
+print(a == 10 or b == 10)
+print(not(a == 10))
+print(not(a == 2))
+
