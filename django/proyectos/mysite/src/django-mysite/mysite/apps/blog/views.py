9626fb93df1561c08d95e1056ec11b440931c2cf
diego <diegoaarondv@gmail.com>
2021-03-07 19:01:47 -0500

    update django 19

--- django/proyectos/mysite/src/django-mysite/mysite/apps/blog/views.py
+++ django/proyectos/mysite/src/django-mysite/mysite/apps/blog/views.py
@@ -7,9 +7,19 @@ from django.core.mail import send_mail
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from taggit.models import Tag
+
+from django.db.models import Count
+
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in[tag])
+
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -20,7 +30,7 @@ def post_list(request):
     except EmptyPage:
         # 
         post = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page , 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page , 'posts': posts, 'tag': tag})
 
 # Utilizando Vistas basadas en clases
 
@@ -71,5 +81,15 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+    post_tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                  'comments': comments,
+                  'new_comment': new_comment, 
+                  'comment_form': comment_form,
+                  'similar_posts': similar_posts})
 
