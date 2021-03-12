bd65b5fc15ad39de81664e0563f4c492c21eed26
diego <diegoaarondv@gmail.com>
2021-03-11 20:33:06 -0500

    update django 35

--- django/proyectos/myproject_base/src/myproject_django/api_basic/urls.py
+++ django/proyectos/myproject_base/src/myproject_django/api_basic/urls.py
@@ -1,9 +1,16 @@
 
 from django.urls import path
 
-from .views import article_list, article_detail
+from rest_framework.routers import DefaultRouter
+
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, GenericAPIView, ArticleViewSet
+
+router = DefaultRouter()
 
 urlpatterns = [
-    path('article/', article_list, name = 'article'),
-    path('detail/<int:pk>/', article_detail, name = 'detail'),
+    #path('article/', article_list, name = 'article'),
+    path('article/', ArticleAPIView.as_view(), name = 'article'),
+    #path('detail/<int:pk>/', article_detail, name = 'detail'),
+    path('detail/<int:id>/', ArticleDetails.as_view(), name = 'detail'),
+    path('generic/article/<int:id>/', GenericAPIView.as_view(), name = 'detail2'),
 ]
