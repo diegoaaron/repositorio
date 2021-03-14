f043619170d514775433b64a4b955c53ce58c340
diego <diegoaarondv@gmail.com>
2021-03-13 22:11:54 -0500

    update django 44

---
+++ django/proyectos/learning_base/src/learning_django/courses/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+
+from ..models import Subject, Course
+from .serializers import SubjectSerializer, CourseSerializer
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class SubjectDetailView(generics.RetrieveAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class CourseListView(generics.ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CourseDetailView(generics.RetrieveAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
