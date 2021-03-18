ddcb7cbb6c4da0cc3a00342926ec5ccb65c408b1
diego <diegoaarondv@gmail.com>
2021-03-17 20:40:34 -0500

    update django 56

--- django/proyectos/survey_base/survey_django/api/urls.py
+++ django/proyectos/survey_base/survey_django/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import include, path
+from django.urls import include, path, patterns
+
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
-urlpatterns = [
-    path('surveys/', views.survey_list, name='survey_list'),
-    path('surveys/<pk>/', views.survey_details, name='survey_detail'),
-]
+
+urlpatterns = patterns('api.views',
+    #path('surveys/', views.survey_list, name='survey_list'),
+    #path('surveys/<pk>/', views.survey_details, name='survey_detail'),
+    path('surveys/', views.SurveyList.as_view(), name='survey_list'),
+    path('surveys/<pk>/', views.SurveyDetails.as_view(), name='survey_detail'),
+)
+urlpatterns = format_suffix_patterns(urlpatterns)
--- django/proyectos/survey_base/survey_django/api/views.py
+++ django/proyectos/survey_base/survey_django/api/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view # primer método
+from rest_framework.views import APIView # segundo métod
 from rest_framework.response import Response
 
 from surveyf.models import Survey
 from.serializers import SurveySerializer
 
+"""
 @api_view(['GET', 'POST'])
 def survey_list(request):
     ''' Listar las encuestas o crear una nueva '''
@@ -58,5 +60,52 @@ def survey_details(request, pk):
         survey.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+"""
 
+class SurveyList(APIView):
+    ''' Lista de todas las encuestas y crear una nueva encuesta'''
+    def get(sef, request, format=None):
+        survey = Survey.objects.all()
+        serializer = SurveySerializer(survey, many=True)
+        
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SurveySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SurveyDetails(APIView):
+    '''obtener, actualizar, eliminar una encuesta específica'''
+    def get_object(self, pk):
+        try:
+            return Survey.objects.get(pk=pk)
+
+        except Survey.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        survey = self.get_object(pk)
+        serializer = SurveySerializer(survey)
+
+        return Response(serializer.data)
     
+    def put(self, request, pk, format=None):
+        survey = self.get_object(pk)
+        serializer = SurveySerializer(survey, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        survey = self.get_object(pk)
+        survey.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
