8cd4a985625d68c9ccf56481cb9b44295f8371b3
diego <diegoaarondv@gmail.com>
2021-03-17 21:12:03 -0500

    update django 57

--- django/proyectos/survey_base/survey_django/api/urls.py
+++ django/proyectos/survey_base/survey_django/api/urls.py
@@ -1,14 +1,11 @@
-from django.urls import include, path, patterns
-
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import include, path
 
 from . import views
 
-
-urlpatterns = patterns('api.views',
+urlpatterns = [
     #path('surveys/', views.survey_list, name='survey_list'),
     #path('surveys/<pk>/', views.survey_details, name='survey_detail'),
     path('surveys/', views.SurveyList.as_view(), name='survey_list'),
     path('surveys/<pk>/', views.SurveyDetails.as_view(), name='survey_detail'),
-)
-urlpatterns = format_suffix_patterns(urlpatterns)
+
+]
