a749287d5d36a7fff2a38208aa0af7634e7789c4
diego <diegoaarondv@gmail.com>
2021-03-26 16:51:22 -0500

    update python 37

--- python/proyectos/hangman/hangman.py
+++ python/proyectos/hangman/hangman.py
@@ -19,15 +19,17 @@ def hangman(word):
     while wrong < len(stages):
         print("\n")
         char = input('Adivina una letra: ')
+        errores = True
 
         for palabra in  rletters:
             if palabra == char:
                 indice = rletters.index(char)
                 board[indice] = char
                 rletters[indice] = '*'
+                errores = False
 
-            else:
-                wrong += 1
+        if errores:
+            wrong += 1
         
         print("palabra formada: ",(" ".join(board)))
         print("errores: ", wrong)
@@ -37,7 +39,7 @@ def hangman(word):
 
         if "_" not in board:
             print("You win! Palabra formada completamente")
-            print(" ".join(board))
+            print('la palabra es', ''.join(board))
             win = True
             break
 
