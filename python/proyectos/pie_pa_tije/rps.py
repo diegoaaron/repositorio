5bdb2e21d7a865bd8d6700feb40e4755f6c999ec
diego <diegoaarondv@gmail.com>
2021-03-26 12:09:38 -0500

    update python 33

--- python/proyectos/pie_pa_tije/rps.py
+++ python/proyectos/pie_pa_tije/rps.py
@@ -5,7 +5,6 @@ t = ["Rock", "Paper", "Scissors"]
 
 # asignando un elemento a la máquina
 computer = t[randint(0, 2)]
-
 player = False
 
 while player == False:
@@ -21,4 +20,21 @@ while player == False:
         else:
             print("You win!", player, "smashes", computer)
     
+    elif player == 'Paper':
+        if computer == 'Scissors':
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    
+    elif player == 'Scissors':
+        if computer == 'Rock':
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
 
+    else:
+        print("That's not a valid play. Check your spelling!")
+
+    # reseteamos los valores para el nuevo intento
+    player = False
+    computer = t[randint(0, 2)]
