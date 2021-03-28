7c9e2fb688e367bfcab06ceb29e41e9a9f81864c
diego <diegoaarondv@gmail.com>
2021-03-28 18:53:03 -0500

    update python 44

---
+++ python/proyectos/snake/temp.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+
+screen_width=700
+screen_height=400
+screen=pygame.display.set_mode([screen_width, screen_height])
+
+white = (255, 255, 255)
+blue = (50, 153, 213)
+
+pygame.draw.rect(screen, white, [500, 200, 20, 20]) # Rect(left, top, width, height) -> Rect
+
+game_over = False
+
+while not game_over:
+    screen.fill(blue)
+    #pygame.draw.rect(screen, white, [500, 200, 20, 20]) # Rect(left, top, width, height) -> Rect
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        
+
+pygame.quit()
+quit()
