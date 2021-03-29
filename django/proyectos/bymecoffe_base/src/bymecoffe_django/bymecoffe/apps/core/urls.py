3ab030a2fb4fb792924e8cf033a94d67b27e77c4
diego <diegoaarondv@gmail.com>
2021-03-29 15:55:27 -0500

    update django 79

--- django/proyectos/bymecoffe_base/src/bymecoffe_django/bymecoffe/apps/core/urls.py
+++ django/proyectos/bymecoffe_base/src/bymecoffe_django/bymecoffe/apps/core/urls.py
@@ -9,6 +9,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('login/', views.login, name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
-    path('social-auth', include('social_django.urls', namespace='social')),
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'), # va
+    path('social-auth', include('social_django.urls', namespace='social')), # va
 ]
